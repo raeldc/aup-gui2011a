e0bcec09ee5745f4e13596daea6825060a0a9aad
Israel Canasa <raeldc@gmail.com>
2011-07-11 16:37:54 +0800

    Added calc.js with basic operations

---
+++ act4/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<head>
+    <title>GUI Programming - Calculator</title>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/mootools/1.3.2/mootools-yui-compressed.js"></script>
+    <script type="text/javascript" src="js/calc.js"></script>
+    <link rel="stylesheet" href="css/reset.css" type="text/css" media="screen" title="Reset CSS" charset="utf-8">
+</head>
+<body>
+    <input type="text" name="screen" value="" id="screen-box" style="text-align:right"> <br />
+    <input type="button" name="multiply" value="x"> <br />
+    <input type="button" name="divide" value="/"> <br />
+    <input type="button" name="add" value="+"> <br />
+    <input type="button" name="subtract" value="-"> <br />
+    <input type="button" name="calculate" value="="> <br />
+</body>
